6c7e52f5136ed21cd5291e6247ccb7c247e8b78f
nmarvila <nichollasmarvila@gmail.com>
2018-06-01 14:53:32 +0000

    refactor app.js

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var commentRoutes     = require("./routes/comments"),
     campgroundRoutes  = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index");
     
-mongoose.connect(process.env.DATABASEURL);
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
+mongoose.connect(url);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
